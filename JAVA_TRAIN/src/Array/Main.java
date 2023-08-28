950f703b35a4f9466838901f5427384d7c21d69e
mahvann <mahvann193@gmail.com>
2023-08-28 17:23:38 +0700

    upload3

---
+++ JAVA_TRAIN/src/Array/Main.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer []a;
+        a = new Integer[10];
+        System.out.println(a[0]);
+        int []b = {1,2,3,4};
+        for (int x:b){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+    }
+}
--- JAVA_TRAIN/src/BTVN1_JAVABACKEND/Main.java
+++ JAVA_TRAIN/src/BTVN1_JAVABACKEND/Main.java
@@ -1,22 +1,23 @@
 package BTVN1_JAVABACKEND;
 
 class MenoryManagement{  //Method area
-    int var1 = 1; //Heap Area
-    double var2 = 2; //Heap Area
-    String var3 = "hello"; //Heap area
-    String var4 = new String("Hello"); //Heap area
-    static int var5 = 12; //Method area
-    static String var6 = "var6"; //Method area
-    static String var7 = new String("var7"); //Method area
+    int var1 = 1; //Heap Area  //Biến instance
+    double var2 = 2; //Heap Area  //Biến instance
+    String var3 = "hello"; //Heap area  //Biến instance
+    String var4 = new String("Hello"); //Heap area  //Biến instance
+    static int var5 = 12; //Method area  //Biến static
+    static String var6 = "var6"; //Method area  //Biến static
+    static String var7 = new String("var7"); //Method area  //Biến static
 
     void myMethod1(int var8 , int var9){ //Stack Area
-        int var10 = 12; //Stack Area
-        String var11 =  "var11"; //Stack Area
-        String var12 = new String("var12"); //Stack Area
+        int var10 = 12; //Stack Area    //Biến local
+        String var11 =  "var11"; //Stack Area    //Biến local
+        String var12 = new String("var12"); //Stack Area    //Biến local
     }
 }
 public class Main {
     public static void main(String [] args){ //Method area
+        int n = 10; //Biến local
         System.out.println("Hello World");
         MenoryManagement var13 = new MenoryManagement(); //Heap area
         var13.myMethod1(14,15);
