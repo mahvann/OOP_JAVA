a416eee7b4a3c1e7fd480e2bb42215d5e7bc31fe
mahvann <mahvann193@gmail.com>
2023-08-28 19:58:04 +0700

    upload test value

--- JAVA_TRAIN/src/BTVN1_JAVABACKEND/Main.java
+++ JAVA_TRAIN/src/BTVN1_JAVABACKEND/Main.java
@@ -4,7 +4,8 @@ class MenoryManagement{  //Method area
     int var1 = 1; //Heap Area  //Biến instance
     double var2 = 2; //Heap Area  //Biến instance
     String var3 = "hello"; //Heap area  //Biến instance
-    String var4 = new String("Hello"); //Heap area  //Biến instance
+    String var4 = new String("Hello"); //Khi dùng từ khóa new tạo một đối tượng mới thì đối tượng này String lưu trong bộ nhớ Heap
+                                                // biến var4 được lưu trong Stack trỏ đến đối tượng này
     static int var5 = 12; //Method area  //Biến static
     static String var6 = "var6"; //Method area  //Biến static
     static String var7 = new String("var7"); //Method area  //Biến static
@@ -16,11 +17,15 @@ class MenoryManagement{  //Method area
     }
 }
 public class Main {
+    // Khi thực hiện chương trình java tìm đến hàm main và hàm main được khởi tạo trong bộ nhớ Stack
     public static void main(String [] args){ //Method area
-        int n = 10; //Biến local
+        int n = 10; //Biến local được khởi tạo và lưu ở Stack
         System.out.println("Hello World");
-        MenoryManagement var13 = new MenoryManagement(); //Heap area
+        MenoryManagement var13 = new MenoryManagement(); //Khi dùng từ khóa new tạo một đối tượng mới thì đối tượng này MenoryManagement lưu trong bộ nhớ Heap
+        //Biến tam chiếu var13 nằm trong bộ nhớ Stack trỏ đến đối tượng MenoryManagement
         var13.myMethod1(14,15);
+        //Hàm myMethod1() được gọi và lưu trong bộ nhớ Stack
+        //Biến var8, var9 là biến local nên được lưu trong Stack
         System.out.println("Hello");
     }
 }
